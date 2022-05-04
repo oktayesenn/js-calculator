51f5d46f20d595809e0c5f49f4dfd3a4ad24cf48
Oktay esen <oktayesenn@gmail.com>
2022-05-04 13:24:46 -0400

    Oktay JS Calc

--- calculator.js
+++ calculator.js
@@ -1,2 +1,18 @@
 let calculator = {
+    add: function (num1 , num2){
+    return num1 + num2 
+    },
+    sub: function (num1 , num2){
+        return num1 - num2
+    },
+    div: function (num1 , num2){
+        return num1 / num2
+    },
+    mul: function (num1 , num2){
+        return num1 * num2
+    },
+    exp: function(num1 , num2){
+        return num1 ** num2
+    }
 }
+
